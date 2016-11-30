8afe5441ad8e21c4a7b7617dc2351e3721101cc2
Emmy <ajemeke@gmail.com>
2016-11-30 04:07:44 +0100

    initial commit

---
+++ resources/config/restricted.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emmy
+ * Date: 11/30/2016
+ * Time: 2:28 AM
+ */
+
+return [
+    'message' =>'That username is taken. Please try another!',
+    'usernames'=>'
+
+admin
+index
+user
+password
+route
+
+'];
---
+++ src/RestrictedUsernameServiceProvider.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emmy
+ * Date: 11/30/2016
+ * Time: 12:51 AM
+ */
+
+namespace Codulab\Username;
+
+use Illuminate\Support\ServiceProvider;
+use Validator;
+
+
+class RestrictedUsernameServiceProvider extends ServiceProvider
+{
+    protected $message = 'That username is taken. Please try another!';
+
+    /**
+     * Publishes the config files.
+     */
+    public function boot()
+    {
+        $message = $this->getMessage();
+        $usernames = $this->getRestrictedUsernames();
+
+        Validator::extend('restricted', function ($attribute, $value, $parameters, $validator) use ($usernames) {
+            return !$usernames->has($value);
+        }, $this->message);
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laravel-username'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getRestrictedUsernames()
+    {
+        $path = public_path('usernames.txt');
+        $data = explode("\r\n", file_get_contents($path));
+        $data = array_map(function($value){
+            return preg_replace("/\s/", "", $value);
+        }, $data);
+        return $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return config('restricted.message') ?: $this->message;
+    }
+}
